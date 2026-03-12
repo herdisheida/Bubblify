10bb390da5f4c0bcb9ac77db276130067965ecd9
Herdis Heida <herdisj24@ru.is>
2026-03-12 21:50:09 +0000

    setup --- copy frá melodia

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -1,5 +1,8 @@
 import Image from "next/image";
+import "../styles/global.css";
+import "../styles/variables.css";
 
+// available bubble products
 export default function Home() {
   return <div> HOME page</div>;
 }

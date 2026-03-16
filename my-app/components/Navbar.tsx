2105bc8005a914d14a5a1d41d10142a9bfbac0f7
Herdis Heida <herdisj24@ru.is>
2026-03-16 23:48:38 +0000

    disable flowbyte link in navbar

--- my-app/components/Navbar.tsx
+++ my-app/components/Navbar.tsx
@@ -22,11 +22,11 @@ export default function Navbar() {
         // </nav>
     <nav className="bg-neutral-primary fixed w-full z-20 top-0 start-0 border-b border-default">
     <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-        <a href="https://flowbite.com/" className="flex items-center space-x-3 rtl:space-x-reverse">
+        <p className="flex items-center space-x-3 rtl:space-x-reverse">
         {/* TODO find logo */}
             <img src="https://flowbite.com/docs/images/logo.svg" className="h-7" alt="Flowbite Logo" />
             <span className="self-center text-xl text-heading font-semibold whitespace-nowrap">Bubblify</span>
-        </a>
+        </p>
 
         <div className="hidden w-full md:block md:w-auto" id="navbar-default">
         <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-default rounded-base bg-neutral-secondary-soft md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-neutral-primary">

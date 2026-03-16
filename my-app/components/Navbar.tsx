fff56e51eb13bbad10eacb7e48ccd55d61f5a690
Herdis Heida <herdisj24@ru.is>
2026-03-16 23:28:26 +0000

    Update Navbar.tsx

--- my-app/components/Navbar.tsx
+++ my-app/components/Navbar.tsx
@@ -1,13 +1,23 @@
+"use client"
+
 import Link from "next/link";
+import Image from "next/image";
+
 
 export default function Navbar() {
   return (
     <nav>
-        <h1>Navbar</h1>
-        <Link href="/bubbles">Products</Link>
-        <Link href="/bundles">Bundles</Link>
-        <Link href="/about">About us</Link>
-        <Link href="/cart">Cart</Link>
+        <div>
+            {/* TODO find logo */}
+            <Image src="/logo.png" alt="Logo" width={100} height={50} />
+        </div>
+
+        <div>
+            <Link href="/bubbles">Products</Link>
+            <Link href="/bundles">Bundles</Link>
+            <Link href="/about">About us</Link>
+            <Link href="/cart">Cart</Link>
+        </div>
     </nav>
   )
 }

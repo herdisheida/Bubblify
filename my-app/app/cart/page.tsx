48b55345893797b89845724a80a72ed59f364db9
Herdis Heida <herdisj24@ru.is>
2026-03-16 23:21:39 +0000

    cart

--- my-app/app/cart/page.tsx
+++ my-app/app/cart/page.tsx
@@ -1,26 +1,34 @@
 // cart management - app/(checkout)/cart/page.tsx
+'use client'
 
-"use client";
-
-import { useState, useEffect } from 'react';
+import { useCart } from "@/context/CartContext"
 
 export default function CartPage() {
-  const [cart, setCart] = useState([]);
-
-  useEffect(() => {
-    const savedCart = localStorage.getItem('bubblify-cart');
-    if (savedCart) setCart(JSON.parse(savedCart));
-  }, []);
-
-  const updateQuantity = (id: number, delta: number) => {
-
-    };
+  const { cart, increaseQuantity, decreaseQuantity, removeFromCart } = useCart()
 
   return (
-    <div className="max-w-2xl mx-auto p-4">
-      <h1 className="text-2xl font-bold">Your Cart</h1>
+    <div>
+      <h1>Your Cart</h1>
       {/* List items, quantities, and total price */}
-      {/* "Proceed to checkout" button */}
+      {cart.map((item) => (
+        <div key={item.bubble.id}>
+          <h2>{item.bubble.name}</h2>
+          <p>Price: ${item.bubble.price}</p>
+          <p>Quantity: {item.quantity}</p>
+
+          {/* remove or decrease */}
+          { cart.length > 0 ? (
+            <button onClick={() => removeFromCart(item.bubble.id)}>Remove</button>
+          ) : <button onClick={() => decreaseQuantity(item.bubble.id)}>-</button> }
+          {/* increase */}
+          <button onClick={() => increaseQuantity(item.bubble.id)}>+</button>
+        </div>
+      ))}
+
+      {/* proceed to checkout button functionality */}
+      {cart.length > 0 && (
+        <button>Proceed to checkout</button>
+      )}
     </div>
   );
 }

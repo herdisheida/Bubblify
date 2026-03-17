6863d39aaa95c0c28001035a451e5aec535e961b
Herdis Heida <herdisj24@ru.is>
2026-03-17 19:31:50 +0000

    remove todo comments

--- my-app/components/Navbar.tsx
+++ my-app/components/Navbar.tsx
@@ -9,7 +9,6 @@ export default function Navbar() {
     <nav className="bg-white fixed w-full z-20 top-0 start-0 border-b border-default shadow-md">
         <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
             <p className="flex items-center space-x-3 rtl:space-x-reverse">
-            {/* TODO find logo */}
                 <img src="https://flowbite.com/docs/images/logo.svg" className="h-7" alt="Flowbite Logo" />
                 <span className="self-center text-xl text-heading font-semibold whitespace-nowrap">Bubblify</span>
             </p>

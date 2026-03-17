21fc9fcb69d57dc41c21da8100df0efe72683f45
Herdis Heida <herdisj24@ru.is>
2026-03-17 20:26:07 +0000

    i don't like the text

--- my-app/app/orders/[telephone]/loading.tsx
+++ my-app/app/orders/[telephone]/loading.tsx
@@ -4,7 +4,7 @@ import LoadingSpinner from "@/components/ui/LoadingSpinner"
 export default function Loading() {
   return (
     <div className="p-10 text-center">
-      <h2 className="text-3xl pb-6">Searching for your orders...</h2>
+      <h2 className="text-3xl pb-6"></h2>
       <LoadingSpinner />
     </div>
   )

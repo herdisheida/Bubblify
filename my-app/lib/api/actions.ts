5e732c3440aef2241973257b26f3f50bea156cb3
Herdis Heida <herdisj24@ru.is>
2026-03-17 12:50:51 +0000

    posting order

--- my-app/lib/api/actions.ts
+++ my-app/lib/api/actions.ts
@@ -1,1 +1,18 @@
-export async function submitOrder(orderData: any, cartData: any) {}
+"use server"
+
+import { Bubble } from "@/types/bubbles"
+import { Order } from "@/types/orders"
+
+
+export async function submitOrder(data: Order, cart: Bubble[]) {
+  await fetch("http://localhost:3500/orders", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      customer: data,
+      items: cart,
+    }),
+  })
+}

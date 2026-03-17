e20182d33169441670e06ecf8eee3c607a2f1436
Herdis Heida <herdisj24@ru.is>
2026-03-17 19:37:51 +0000

    cooler bubble animatin cart

--- my-app/components/BubbleCard.tsx
+++ my-app/components/BubbleCard.tsx
@@ -4,20 +4,21 @@ import Link from 'next/link';
 
 export default function BubbleCard({ bubble }: { bubble: Bubble }) {
     return (
-        <div className="border p-4 rounded-xl">
-            <Link href={`/bubbles/${bubble.id}`} key={bubble.id}>
-            <div className="justify-self-center">
-                <Image 
-                src={bubble.image} 
-                alt={bubble.name} 
-                width={150}
-                height={150}
-                priority
-                />
+        <Link href={`/bubbles/${bubble.id}`} key={bubble.id}>
+            <div className="border p-4 rounded-xl hover:scale-101 hover:shadow-md transition-transform cursor-pointer">
+                <div className="justify-self-center">
+                    <Image 
+                    src={bubble.image} 
+                    alt={bubble.name} 
+                    width={150}
+                    height={150}
+                    priority
+                    />
+                </div>
+                <h2 className="text-xl font-semibold justify-self-center">{bubble.name}</h2>
+                <p className="justify-self-center">${bubble.price}</p>
             </div>
-            <h2 className="text-xl font-semibold justify-self-center">{bubble.name}</h2>
-            <p className="justify-self-center">${bubble.price}</p>
-            </Link>
-        </div>
+        </Link>
+
     )
 }

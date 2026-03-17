2160b6178e3cff60d3006a8d6fbadf9c1757a95a
Herdis Heida <herdisj24@ru.is>
2026-03-17 21:29:20 +0000

    semi homepage

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -6,7 +6,8 @@ import "../styles/variables.css";
 export default function HomePage() {
   return (
   <div className="p-10">
-    <h1 className="text-3xl pb-6">HOME page</h1>
+    <h1 className="text-3xl pb-6">Home page</h1>
+    <p>Welcome to our bubble shop!</p>
   </div>
 );
 }

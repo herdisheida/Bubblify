5b47d0f02b6563c91fd550500637da940aa77bd3
Herdis Heida <herdisj24@ru.is>
2026-03-17 21:01:17 +0000

    prettier delivery method

--- my-app/app/(checkout)/checkout/delivery/page.tsx
+++ my-app/app/(checkout)/checkout/delivery/page.tsx
@@ -18,20 +18,24 @@ export default function DeliveryPage({}) {
 
   return (
     <div className="p-10">
-      <h1 className="text-3xl pb-6">Delivery Method</h1>
-      <button className="bg-sky-500 hover:bg-sky-700 text-white px-6 py-3 rounded-xl mt-4 mr-4 transition-colors
+      <h1 className="text-3xl pb-6">Checkout</h1>
+      <h2 className="pb-6">Please choose a delivery method</h2>
+
+      <div className="grid grid-rows-2 gap-8">
+      <button className="bg-sky-500 hover:bg-sky-700 text-white px-6 py-3 rounded-xl transition-colors
                           cursor-pointer hover:scale-101 hover:shadow-md
                           active:outline-2 active:outline-offset-2 active:outline-indigo-500 active:opacity-80"
         onClick={() => handleSelect(pickup)}>
           Store Pickup
       </button>
 
-      <button className="bg-sky-500 hover:bg-sky-700 text-white px-6 py-3 rounded-xl mt-4 ml-4 transition-colors
+      <button className="bg-sky-500 hover:bg-sky-700 text-white px-6 py-3 rounded-xl transition-colors
                           cursor-pointer hover:scale-101 hover:shadow-md
                           active:outline-2 active:outline-offset-2 active:outline-indigo-500 active:opacity-80"
         onClick={() => handleSelect(delivery)}>
           Home Delivery
       </button>
+      </div>
     </div>
   );
 }

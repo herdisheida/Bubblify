dc9c71faa9c3c2832055439f2fdc7ab114f65a7c
Herdis Heida <herdisj24@ru.is>
2026-03-17 18:58:23 +0000

    bubble items in middle

--- my-app/components/BubbleCard.tsx
+++ my-app/components/BubbleCard.tsx
@@ -6,7 +6,7 @@ export default function BubbleCard({ bubble }: { bubble: Bubble }) {
     return (
         <div className="border p-4 rounded-xl">
             <Link href={`/bubbles/${bubble.id}`} key={bubble.id}>
-            <div>
+            <div className="justify-self-center">
                 <Image 
                 src={bubble.image} 
                 alt={bubble.name} 
@@ -15,8 +15,8 @@ export default function BubbleCard({ bubble }: { bubble: Bubble }) {
                 priority
                 />
             </div>
-            <h2 className="text-xl font-semibold">{bubble.name}</h2>
-            <p>${bubble.price}</p>
+            <h2 className="text-xl font-semibold justify-self-center">{bubble.name}</h2>
+            <p className="justify-self-center">${bubble.price}</p>
             </Link>
         </div>
     )

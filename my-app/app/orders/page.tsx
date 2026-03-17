9b1d9e8d021d86ead701649525ebf9f641465a50
Herdis Heida <herdisj24@ru.is>
2026-03-17 08:49:52 +0000

    todo

--- my-app/app/orders/page.tsx
+++ my-app/app/orders/page.tsx
@@ -1,10 +1,16 @@
 // look at past orders (allow searching telephone numbers) - http://localhost:3000/orders
 
 export default function OrdersPage({}) {
+
   return (
   <div className="p-10">
     <h1 className="text-3xl mb-6">Orders</h1>
     <p>Here you can view your past orders.</p>
+
+    {/* TODO loop through orders */}
+    {/* TODO add search by telephone functionality */}
+    {/* TODO add checkout button */}
+    <button></button>
   </div>
 );
 }

65dda4a973cbb6496bdcdc531de4373bce210b1b
Herdis Heida <herdisj24@ru.is>
2026-03-17 12:56:41 +0000

    success pae

--- my-app/app/(checkout)/checkout/success/page.tsx
+++ my-app/app/(checkout)/checkout/success/page.tsx
@@ -1,11 +1,8 @@
-
-
-export default function SuccessPage({}) {
-
+export default function SuccessPage() {
   return (
     <div className="p-10">
-      <h1 className="text-3xl mb-6">Success Page</h1>
-
+      <h1 className="text-3xl mb-6">Order Successful</h1>
+      <p>Your bubbles are on the way!</p>
     </div>
-  );
+  )
 }

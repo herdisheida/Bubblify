e2a704d547e89e851dfa02b4985a1127ca76782f
Herdis Heida <herdisj24@ru.is>
2026-03-17 19:12:32 +0000

    spacing

--- my-app/components/BundleCard.tsx
+++ my-app/components/BundleCard.tsx
@@ -18,16 +18,16 @@ export default function BundleCard({ bundle, bubbles }: BundleCardProps) {
 
   return (
     <div className="border p-4 rounded-lg">
-      <h2 className="text-xl font-semibold justify-self-center">{bundle.name}</h2>
+      <h2 className="text-xl font-semibold justify-self-center pb-4 pt-2">{bundle.name}</h2>
 
-      <ul className="mt-2 justify-self-center">
+      <ul className="mt-2">
         {/* list of bubble names in the bundle */}
         {bundleBubbles.map((bubble) =>
           bubble ? 
           (
             <div key={bubble.id} className="flex items-center gap-2">
                 <Image src={bubble.image} alt={bubble.name} width={50} height={50} priority />
-                <li key={bubble.id} className="justify-self-center">
+                <li key={bubble.id}>
                   {bubble.name}
                 </li>
             </div>

de72f4d45cfd35373cee26fc799efd04d44feed1
Herdis Heida <herdisj24@ru.is>
2026-03-17 20:37:16 +0000

    loading on bubble page and bundle page

---
+++ my-app/app/(shop)/bubbles/loading.tsx
@@ -0,0 +1,11 @@
+// loading while GETting bubbles - app/bubbles/loading.tsx
+import LoadingSpinner from "@/components/ui/LoadingSpinner"
+
+export default function Loading() {
+  return (
+    <div className="p-10 text-center">
+      <h2 className="text-3xl pb-6"></h2>
+      <LoadingSpinner />
+    </div>
+  )
+}
